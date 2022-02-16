625f3c0624c2456f0891bbd67ca1e2eb84331dfb
Lóránt Pintér <lorant@gradle.com>
2022-02-16 23:23:30 +0100

    First version

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import VlookupConverter from './components/VlookupConverter.vue'
+</script>
+
+<template>
+  <main>
+    <VlookupConverter />
+  </main>
+</template>
+
+<style>
+@import './assets/base.css';
+
+main {
+  padding: 1rem;
+
+  font-weight: normal;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+</style>
---
+++ src/components/VlookupConverter.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <label>VLOOKUP:</label>
+  <input />
+</template>
+
+<style scoped>
+input {
+  font-family: monospace;
+}
+</style>
